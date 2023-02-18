333697dc0e5555ee7ad6773f3c1ca47cf13bc241
gioocharkviani <gioocharkviani@gmail.com>
2023-02-18 13:38:54 +0400

    fdgfdgfdsG

--- src/header/NavMenu.js
+++ src/header/NavMenu.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NavMenu = () => {
   return (
     <div className='flex items-center '>
-      <ul className='flex text-[#FAFAFA] text-[24px] font-FiraGO gap-[48px]'>
+      <ul className='flex text-[#FAFAFA] text-[24px] font-FiraGO font-[300] gap-[48px]'>
         <li><a href='' >როგორ მუშაობს?</a></li>
         <li><a href='' >რატომ ეს?</a></li>
         <li><a href='' >ასისტენტები</a></li>
